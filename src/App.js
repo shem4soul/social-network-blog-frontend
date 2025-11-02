db2ff703bee4099fd850194a58c41b3e4292b5ea
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-02 19:02:26 +0100

    Set initial authentication state to true

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
   state = {
     showBackdrop: false,
     showMobileNav: false,
-    isAuth: false,
+    isAuth: true,
     token: null,
     userId: null,
     authLoading: false,
