450ec36835a0c89e0be355b6e619668ae0a0d6a0
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-09 04:33:45 +0100

    refactor delete post

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -124,7 +124,8 @@ class Feed extends Component {
     let url = "http://localhost:8080/feed/post";
     let method = "POST";
     if (this.state.editPost) {
-      url = "URL";
+      url = "http://localhost:8080/feed/post/" + this.state.editPost._id;
+      method = 'PUT';
     }
 
     fetch(url, {
@@ -181,7 +182,9 @@ class Feed extends Component {
 
   deletePostHandler = (postId) => {
     this.setState({ postsLoading: true });
-    fetch("URL")
+    fetch("http://localhost:8080/feed/post/" + postId, {
+      method: 'DELETE'
+    })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Deleting a post failed!");
