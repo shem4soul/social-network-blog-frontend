8d07b2b85ced5456193b9c6bc9d3c7cfdd345cd1
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-12 03:55:37 +0100

    added bearere token in the header for authentication

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -53,7 +53,11 @@ class Feed extends Component {
       this.setState({ postPage: page });
     }
 
-    fetch("http://localhost:8080/feed/posts?page=" + page)
+    fetch("http://localhost:8080/feed/posts?page=" + page, {
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
+    })
       .then((res) => {
         if (res.status !== 200) {
           throw new Error("Failed to fetch posts.");
