1c5a6bf8470f41c169082a62d30006993e2813b9
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-12 18:34:32 +0100

    added authorization header

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -135,6 +135,9 @@ class Feed extends Component {
     fetch(url, {
       method: method,
       body: formData,
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
@@ -187,7 +190,10 @@ class Feed extends Component {
   deletePostHandler = (postId) => {
     this.setState({ postsLoading: true });
     fetch("http://localhost:8080/feed/post/" + postId, {
-      method: 'DELETE'
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
